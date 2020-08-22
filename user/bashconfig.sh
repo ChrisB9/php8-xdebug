da9e510c19ce791e4b91617a4afd16b5fcb5ac27
Chris Ben <chris.ben9@gmail.com>
2020-08-22 18:24:10 +0200

    Add node and yarn alias to bashrc

--- user/bashconfig.sh
+++ user/bashconfig.sh
@@ -3,8 +3,23 @@ export PATH=$PATH:~/.composer/vendor/bin:./bin:./vendor/bin:./node_modules/.bin
 source ~/.git-completion.bash
 source ~/.git-prompt.sh
 
-# only enable docker aliases if docker-socket is mounted
+# for more information on this: https://github.com/pluswerk/php-dev/blob/master/.additional_bashrc.sh
 CONTAINER_ID=$(basename $(cat /proc/1/cpuset))
+export HOST_DISPLAY_NAME=$HOSTNAME
+if sudo docker ps -q &>/dev/null; then
+  DOCKER_COMPOSE_PROJECT=$(sudo docker inspect ${CONTAINER_ID} | grep '"com.docker.compose.project":' | awk '{print $2}' | tr --delete '"' | tr --delete ',')
+  export NODE_CONTAINER=$(sudo docker ps -f "name=${DOCKER_COMPOSE_PROJECT}_node_1" --format {{.Names}})
+  export HOST_DISPLAY_NAME=$(sudo docker inspect ${CONTAINER_ID} --format='{{.Name}}')
+  export HOST_DISPLAY_NAME=${HOST_DISPLAY_NAME:1}
+
+  alias node_exec='sudo docker exec -u $(id -u):$(id -g) -w $(pwd) -it ${NODE_CONTAINER}'
+  alias node_root_exec='sudo docker exec -w $(pwd) -it ${NODE_CONTAINER}'
+
+  alias node='node_exec node'
+  alias npm='node_exec npm'
+  alias npx='node_exec npx'
+  alias yarn='node_exec yarn'
+fi;
 export HOST_DISPLAY_NAME=$HOSTNAME
 
 if [[ $CONTAINER_ID != ${HOSTNAME}* ]] ; then
@@ -13,7 +28,6 @@ fi
 
 PS1='\033]2;'$(pwd)'\007\[\e[0;36m\][\[\e[1;31m\]\u\[\e[0;36m\]@\[\e[1;34m\]$HOST_DISPLAY_NAME\[\e[0;36m\]: \[\e[0m\]\w\[\e[0;36m\]]\[\e[0m\]\$\[\e[1;32m\]\s\[\e[0;33m\]$(__git_ps1)\[\e[0;36m\]> \[\e[0m\]\n$ ';
 
-# Run SSH Agent and add key 7d
 if [ -z "$SSH_AUTH_SOCK" ] ; then
   ssh-add -t 604800 ~/.ssh/id_rsa
 fi
