a3888c8a7479f521ecda6a2f8b1a38272b409a11
Christian Rodriguez Benthake <git@cben.co>
2020-12-30 16:30:52 +0100

    fix condition in entrypoint installation routine

--- entrypoint/scripts/docker_install.sh
+++ entrypoint/scripts/docker_install.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-if [ -e /var/run/docker.sock ] && [ -z "$IS_ALPINE" ]; then
+if [ -e /var/run/docker.sock ] && [ -n "$IS_ALPINE" ]; then
    apk add docker
 fi
-if [ -e /var/run/docker.sock ] && [ -n "$IS_ALPINE" ]; then
+if [ -e /var/run/docker.sock ] && [ -z "$IS_ALPINE" ]; then
    apt update && apt install docker
 fi
